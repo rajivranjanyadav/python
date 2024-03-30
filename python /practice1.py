4e52d726afd50ea81a9de6366439296f74375066
rajivranjanyadav <160840661+rajivranjanyadav@users.noreply.github.com>
2024-03-30 11:36:01 +0000

    practice done

---
+++ python /practice1.py
@@ -0,0 +1,17 @@
+# print("Rajiv")
+# num1=int(input("enter number"))
+# # print("This is your number",num1)
+
+# #Write a python program to calculate area of a rectangle
+# length=eval(input("enter length "))
+# breadth=eval(input("enter breadth"))
+# result=length*breadth
+# print(result)
+
+#WAP to calculate simple interest 
+# principle=int(input("Enter principle"))
+# Rate=int(input("enter Rate"))
+# time=int(input("enter time"))
+# results=(principle*Rate*time)/100
+# print("Simple interest is:-", results)
+
